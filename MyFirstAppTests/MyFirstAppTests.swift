8638ea9f108f49899d862dd8a4c0887675236683
Nick Dunn <bassthing@googlemail.com>
2026-07-10 13:57:00 +0100

    Initial Commit

---
+++ MyFirstAppTests/MyFirstAppTests.swift
@@ -0,0 +1,19 @@
+//
+//  MyFirstAppTests.swift
+//  MyFirstAppTests
+//
+//  Created by Nick D on 10/07/2026.
+//
+
+import Testing
+@testable import MyFirstApp
+
+struct MyFirstAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
